1314a77dbb1c5a536dfbcf38f206999b3bb26f9c
llwslc <llwslc@gmail.com>
2024-12-02 04:05:26 +0800

    Create pass.js

---
+++ pass.js
@@ -0,0 +1,20 @@
+module.exports = [
+  // fans
+  'SANTOS',
+  'OG',
+  'PSG',
+  'BAR',
+  'ALPINE',
+  'CITY',
+  'LAZIO',
+  'ASR',
+  'PORTO',
+  'JUV',
+  'ACM',
+  'ATM',
+
+  // eur
+  'EUR',
+  'AEUR',
+  'EURI'
+];
